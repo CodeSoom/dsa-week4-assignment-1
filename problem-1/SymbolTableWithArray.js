7f97388401c4a47456ea9e880d0888f1312720f4
whoodongpyo <vvhooping@gmail.com>
2024-02-01 08:24:23 +0900

    solve: 비순차 배열 기반 심볼 테이블 구현하기

--- problem-1/SymbolTableWithArray.js
+++ problem-1/SymbolTableWithArray.js
@@ -1,4 +1,69 @@
 class SymbolTable {
+  #keys = [];
+
+  #values = [];
+
+  #numberOfItems = 0;
+
+  constructor(count = 20) {
+    this.#keys = new Array(count);
+    this.#values = new Array(count);
+  }
+
+  put(key, value) {
+    for (let index = 0; index < this.#numberOfItems; index++) {
+      if (key === this.#keys[index]) {
+        this.#values[index] = value;
+        return;
+      }
+    }
+
+    this.#keys[this.#numberOfItems] = key;
+    this.#values[this.#numberOfItems] = value;
+    this.#numberOfItems += 1;
+  }
+
+  get(key) {
+    for (let index = 0; index < this.#numberOfItems; index++) {
+      if (key === this.#keys[index]) {
+        return this.#values[index];
+      }
+    }
+  }
+
+  delete(key) {
+    for (let index = 0; index < this.#numberOfItems; index++) {
+      if (key === this.#keys[index]) {
+        for (let j = index; j < this.#numberOfItems - 1; j++) {
+          this.#keys[j] = this.#keys[j + 1];
+          this.#values[j] = this.#values[j + 1];
+        }
+
+        this.#numberOfItems -= 1;
+        return;
+      }
+    }
+  }
+
+  contains(key) {
+    return !!this.get(key);
+  }
+
+  size() {
+    return this.#numberOfItems;
+  }
+
+  isEmpty() {
+    return this.#numberOfItems === 0;
+  }
+
+  keys() {
+    return [...this.#keys];
+  }
+
+  values() {
+    return [...this.#values];
+  }
 }
 
 module.exports = {
